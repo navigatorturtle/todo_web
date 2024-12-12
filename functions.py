e39924270210996c2e1c2c61b66fe05e1ff24019
Ramajogi-Malla-epam <ramajogi_malla@epam.com>
2024-12-11 21:47:11 -0500

    Initial commit

---
+++ functions.py
@@ -0,0 +1,26 @@
+import os
+FILEPATH = 'todos.txt'
+if not os.path.exists(FILEPATH):
+    with open(FILEPATH,'w') as file:
+        pass
+
+
+def get_todos(filepath=FILEPATH):
+    """
+    Reads a text file and returns the todo list
+    :param filepath:
+    :return: todo list
+    """
+    with open(filepath, 'r') as file_local:
+        todos_local = file_local.readlines()
+    return todos_local
+
+def write_todos_to_text_file(todos_arg,filepath=FILEPATH):
+    with open(filepath, 'w') as file_local:
+        file_local.writelines(todos_arg)
+
+
+if __name__ == "__main__":
+    print(get_todos())
+
+
---
+++ todo_web.py
@@ -0,0 +1,24 @@
+import streamlit as st
+import functions
+
+
+todos = functions.get_todos()
+def add_todo():
+    newtodo = st.session_state["new_todo"]
+    todos.append(newtodo+"\n")
+    functions.write_todos_to_text_file(todos)
+    st.session_state["new_todo"] = ""
+
+st.title("My todo App")
+st.write("This app will increase your productivity")
+
+for index,todo in enumerate(todos):
+    checkbox = st.checkbox(todo,key=f"{todo}{index}")
+    if checkbox:
+        todos.pop(index)
+        functions.write_todos_to_text_file(todos)
+        del st.session_state[f"{todo}{index}"]
+        st.rerun()
+
+st.text_input(label="Enter new todo",placeholder="Enter a todo",
+              on_change=add_todo, key= "new_todo",label_visibility="hidden" )
